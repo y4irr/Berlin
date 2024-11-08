3d8dd275ae5bebcd3d0d69d2fdeb0193f36ea27b
Yair Soto <y4ir180919@gmail.com>
2024-11-07 21:23:50 -0300

    chore: optimize CustomTablistProvider class improving the code styling

--- src/main/java/dev/astro/net/tablist/adapter/impl/CustomTablistProvider.java
+++ src/main/java/dev/astro/net/tablist/adapter/impl/CustomTablistProvider.java
@@ -20,8 +20,7 @@ import java.util.List;
 
 public class CustomTablistProvider implements TabAdapter {
 
-    private final List<CustomSkin> customSkins;
-
+    private final List<CustomSkin> customSkins = new ArrayList<>();
     private final FileConfig tabFile;
     private final HCFCoreManager hcfCoreManager;
     private final ServerDataManager serverDataManager;
@@ -30,66 +29,59 @@ public class CustomTablistProvider implements TabAdapter {
         this.tabFile = plugin.getTablistFile();
         this.hcfCoreManager = plugin.getHcfCoreManager();
         this.serverDataManager = plugin.getServerDataManager();
-
-        this.customSkins = new ArrayList<>();
-
-        this.loadCustomSkins(plugin.getTablistHeadsFile().getConfiguration());
+        loadCustomSkins(plugin.getTablistHeadsFile().getConfiguration());
     }
 
     private void loadCustomSkins(FileConfiguration config) {
-        if (config.contains("heads")) {
-            ConfigurationSection skinsSection = config.getConfigurationSection("heads");
-            for (String key : skinsSection.getKeys(false)) {
+        ConfigurationSection skinsSection = config.getConfigurationSection("heads");
+        if (skinsSection != null) {
+            skinsSection.getKeys(false).forEach(key -> {
                 ConfigurationSection skinSection = skinsSection.getConfigurationSection(key);
-                String value = skinSection.getString("value");
-                String signature = skinSection.getString("signature");
-                this.customSkins.add(new CustomSkin(key, value, signature));
-            }
+                if (skinSection != null) {
+                    customSkins.add(new CustomSkin(
+                            key,
+                            skinSection.getString("value", ""),
+                            skinSection.getString("signature", "")
+                    ));
+                }
+            });
         }
     }
 
     private Skin getCustomSkin(String name) {
-        return this.customSkins.stream().filter(skin -> skin.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
+        return customSkins.stream()
+                .filter(skin -> skin.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
     }
 
-    protected String replace(Player player, String text) {
-        if (text == null || text.isEmpty()) {
-            return "";
-        }
+    private String replace(Player player, String text) {
+        if (text == null || text.isEmpty()) return "";
+
+        IServerData serverData = serverDataManager.getServerData();
 
         if (text.contains("-status>")) {
             String leftText = text.split("<")[0];
-            String serverName = text
-                    .replace(leftText, "")
-                    .replace("<", "")
-                    .replace("-status>", "");
-
-            IServerData serverData = serverDataManager.getServerData();
+            String serverName = text.replace(leftText, "").replace("<", "").replace("-status>", "");
             String placeholderPath = "<" + serverName + "-status>";
 
-            if (serverData.isOffline(serverName)) return text
-                    .replace(placeholderPath, "&cOffline");
-            else if (serverData.isWhitelisted(serverName)) return text
-                    .replace(placeholderPath, "&eWhitelisted");
-            else if (serverData.isMaintenance(serverName)) return text
-                    .replace(placeholderPath, "&cMaintenance");
-            else return text.replace(placeholderPath, "&aOnline");
+            text = text.replace(placeholderPath,
+                    serverData.isOffline(serverName) ? "&cOffline" :
+                            serverData.isWhitelisted(serverName) ? "&eWhitelisted" :
+                                    serverData.isMaintenance(serverName) ? "&cMaintenance" : "&aOnline");
         }
 
-        if (text.contains("<player-name>")) {
-            text = text.replace("<player-name>", player.getName());
-        }
+        text = text.replace("<player-name>", player.getName());
 
         if (hcfCoreManager.getHcf() != null && hcfCoreManager.getHcf().isConnected()) {
-            text = text
-                    .replace("<hcfcore-hcf-kills>", String.valueOf(hcfCoreManager.getHcf().getKills(player.getUniqueId())))
+            text = text.replace("<hcfcore-hcf-kills>", String.valueOf(hcfCoreManager.getHcf().getKills(player.getUniqueId())))
                     .replace("<hcfcore-hcf-deaths>", String.valueOf(hcfCoreManager.getHcf().getDeaths(player.getUniqueId())))
                     .replace("<hcfcore-hcf-lives>", String.valueOf(hcfCoreManager.getHcf().getLives(player.getUniqueId())))
                     .replace("<hcfcore-hcf-deathban>", hcfCoreManager.getHcf().getDeathban(player.getUniqueId()));
         }
+
         if (hcfCoreManager.getKitmap() != null && hcfCoreManager.getKitmap().isConnected()) {
-            text = text
-                    .replace("<hcfcore-kitmap-kills>", String.valueOf(hcfCoreManager.getKitmap().getKills(player.getUniqueId())))
+            text = text.replace("<hcfcore-kitmap-kills>", String.valueOf(hcfCoreManager.getKitmap().getKills(player.getUniqueId())))
                     .replace("<hcfcore-kitmap-deaths>", String.valueOf(hcfCoreManager.getKitmap().getDeaths(player.getUniqueId())))
                     .replace("<hcfcore-kitmap-killstreak>", String.valueOf(hcfCoreManager.getKitmap().getKillStreak(player.getUniqueId())));
         }
@@ -107,49 +99,30 @@ public class CustomTablistProvider implements TabAdapter {
         return replace(player, tabFile.getString("tablist.footer"));
     }
 
-
     @Override
     public List<TabEntry> getLines(Player player) {
         List<TabEntry> entries = new ObjectArrayList<>();
+        addTabEntries(entries, "left", 0, player);
+        addTabEntries(entries, "middle", 1, player);
+        addTabEntries(entries, "right", 2, player);
+        addTabEntries(entries, "far_right", 3, player);
 
-        tabFile.getConfiguration().getConfigurationSection("tablist.lines.left").getKeys(false)
-                .forEach(tabRow -> {
-                    int row = Integer.parseInt(tabRow) - 1;
-                    String text = tabFile.getString("tablist.lines.left." + tabRow + ".text");
-                    String skin = tabFile.getString("tablist.lines.left." + tabRow + ".head");
-                    entries.add(new TabEntry(0, row, replace(player, ChatUtil.translate(text))).setPing(-1)
-                            .setSkin(getCustomSkin(skin)));
-                });
-
-        tabFile.getConfiguration().getConfigurationSection("tablist.lines.middle").getKeys(false)
-                .forEach(tabRow -> {
-                    int row = Integer.parseInt(tabRow) - 1;
-                    String text = tabFile.getString("tablist.lines.middle." + tabRow + ".text");
-                    String skin = tabFile.getString("tablist.lines.middle." + tabRow + ".head");
-                    entries.add(new TabEntry(1, row, replace(player, ChatUtil.translate(text))).setPing(-1)
-                            .setSkin(getCustomSkin(skin)));
-                });
-
-        tabFile.getConfiguration().getConfigurationSection("tablist.lines.right").getKeys(false)
-                .forEach(tabRow -> {
-                    int row = Integer.parseInt(tabRow) - 1;
-                    String text = tabFile.getString("tablist.lines.right." + tabRow + ".text");
-                    String skin = tabFile.getString("tablist.lines.right." + tabRow + ".head");
-                    entries.add(new TabEntry(2, row, replace(player, ChatUtil.translate(text))).setPing(-1)
-                            .setSkin(getCustomSkin(skin)));
-                });
-
-        tabFile.getConfiguration().getConfigurationSection("tablist.lines.far_right").getKeys(false)
-                .forEach(tabRow -> {
-                    int row = Integer.parseInt(tabRow) - 1;
-                    String text = tabFile.getString("tablist.lines.far_right." + tabRow + ".text");
-                    String skin = tabFile.getString("tablist.lines.far_right." + tabRow + ".head");
-                    entries.add(new TabEntry(3, row, replace(player, ChatUtil.translate(text))).setPing(-1)
-                            .setSkin(getCustomSkin(skin)));
-                });
-
-        this.customSkins.add(new CustomSkin("player", Skin.getPlayer(player).getValue(), Skin.getPlayer(player).getSignature()));
+        customSkins.add(new CustomSkin("player", Skin.getPlayer(player).getValue(), Skin.getPlayer(player).getSignature()));
 
         return entries;
+    }
+
+    private void addTabEntries(List<TabEntry> entries, String section, int column, Player player) {
+        ConfigurationSection tabSection = tabFile.getConfiguration().getConfigurationSection("tablist.lines." + section);
+        if (tabSection != null) {
+            tabSection.getKeys(false).forEach(tabRow -> {
+                int row = Integer.parseInt(tabRow) - 1;
+                String text = tabFile.getString("tablist.lines." + section + "." + tabRow + ".text");
+                String skin = tabFile.getString("tablist.lines." + section + "." + tabRow + ".head");
+                entries.add(new TabEntry(column, row, replace(player, ChatUtil.translate(text)))
+                        .setPing(-1)
+                        .setSkin(getCustomSkin(skin)));
+            });
+        }
     }
 }
